e0fc74441106c0ff311ca80a4087231e056b22a2
Khang <banhbeovodung01@gmail.com>
2022-01-22 15:26:00 +0700

    Feat(filter & sharpen)

--- main.m
+++ main.m
@@ -1,11 +1,15 @@
+%% Bai 2
 % (I - f1)/(f2 - f1)
 close all; % Class all figures
 
 % [file_X, folder] = uigetfile({'*.dcm; *.png; *.jpg'}, 'MultiSelect', 'on');
 % file = [folder file_X];
+% I = imread(file);
 
-a = 'H:\My Drive\Khang\BK\212\AS3055 - xlhhysbkts\bt-xlhhbkts\sample data _ chapter 02\lung.bmp';
+a = 'H:\My Drive\Khang\BK\212\AS3055 - xlhhysbkts\sample data _ chapter 02\BW.png';
 I = imread(a);
+
+
 [row, col]= size(I);
 if size(I, 3) == 3
     I = rgb2gray(I);
@@ -19,11 +23,31 @@ t_in = 52;
 t_out = 210;
 
 im_adjust = imadjust(I, [t_in/255 t_out/255], [0 1]);
-figure; 
-subplot(2,1,1); imshow(im_adjust);
-subplot(2,1,2); imhist(im_adjust); title('imadjust');
+imtool(im_adjust);
+% subplot(2,1,1); 
+% subplot(2,1,2); imhist(im_adjust); title('imadjust');
+%% Bai 3
+[file_X, folder] = uigetfile({'*.dcm; *.png; *.jpg'}, 'MultiSelect', 'on');
+file = [folder file_X];
+I = imread(file);
+if size(I, 3) == 3
+    I = rgb2gray(I);
+end
+%% Loc Gauss
+blurred = imgaussfilt(I, 5);
+imshowpair(I, blurred, 'montage');
+%% unsharp
+unsharp = I - blurred;
+imshow(unsharp);
+%% adapthisteq
+hco = adapthisteq(I);
+imshowpair(I, hco, 'montage');
+%% higher contrast original Bai 2
+hco = imadjust(I, [93/255 132/255], [0 1]);
+imshowpair(I, hco, 'montage');
+%% sharpened final image
 
-abc = alter_imadjust(a, 52, 210, 0, 255);
-figure; 
-subplot(2,1,1); imshow(abc);
-subplot(2,1,2); imhist(abc); title('alter_imadjust');
+% sharpened = hco + unsharp + I;
+delta_value = hco - I;
+sharpened = delta_value + unsharp + I;
+imshowpair(I, sharpened, 'montage');
--- sample data _ chapter 02/day1.m
+++
@@ -1,4 +0,0 @@
-% I f1 - f2
-% O I - f1 / (f2 - f1)
-A = imread('xray.jpg');
-% A = adapthisteq
