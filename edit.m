9143ea165d5e354f55e51b541210c1acc794397f
vinguyen290801 <vi.nguyen290801@gmail.com>
2022-01-24 13:44:03 +0700

    chore(edit.m)

--- edit.m
+++ edit.m
@@ -22,7 +22,7 @@ function varargout = edit(varargin)
 
 % Edit the above text to modify the response to help edit
 
-% Last Modified by GUIDE v2.5 23-Jan-2022 11:58:49
+% Last Modified by GUIDE v2.5 24-Jan-2022 12:35:49
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -73,9 +73,9 @@ function varargout = edit_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in Upload_Image.
-function Upload_Image_Callback(hObject, eventdata, handles)
-% hObject    handle to Upload_Image (see GCBO)
+% --- Executes on button press in Select_Image.
+function Select_Image_Callback(hObject, eventdata, handles)
+% hObject    handle to Select_Image (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 global im
@@ -96,6 +96,7 @@ global im
     end
 assignin('base', 'Image', im);
 axes(handles.axes2); imhist(im); title('Histogram');
+
 
 % --- Executes on button press in Delete.
 function Delete_Callback(hObject, eventdata, handles)
@@ -203,19 +204,81 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-% --- Executes on button press in Chuyen.
-function Chuyen_Callback(hObject, eventdata, handles)
-% hObject    handle to Chuyen (see GCBO)
+% --- Executes on button press in Apply.
+function Apply_Callback(hObject, eventdata, handles)
+% hObject    handle to Apply (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 global im
-T1 =get(handles.edit1,'String');
+T1 = get(handles.edit1,'String');
 T1 = str2num(T1);
-T2=get(handles.edit2,'String');
+T2 = get(handles.edit2,'String');
 T2 = str2num(T2);
 im_adjust = imadjust(im, [T1/255 T2/255], [0 1]);
 axes(handles.axes3);
 imshow(im_adjust);
 handles.o=im_adjust;
 guidata(hObject, handles);
-axes(handles.axes4);imhist(im); title('Histogram');
+axes(handles.axes4);imhist(im_adjust); title('Histogram');
+
+% --- Executes on button press in Select_Orginal.
+function Select_Orginal_Callback(hObject, eventdata, handles)
+% hObject    handle to Select_Orginal (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global im_orginal;
+[filename, pathname] = uigetfile('*.*', 'Pick a MATLAB code file');
+    if isequal(filename,0) || isequal(pathname,0)%help uigetfile
+       disp('User pressed cancel')
+    else
+       filename = [pathname filename];
+       im_orginal = imread(filename);
+%        Check if RGB
+       if size(im_orginal, 3) == 3
+            im_orginal = rgb2gray(im_orginal);
+       end
+       axes(handles.axes5);
+       imshow(im_orginal);
+       handles.o=im_orginal;
+       guidata(hObject, handles);
+    end
+assignin('base', 'Image', im_orginal);
+
+
+% --- Executes on button press in Blurred.
+function Blurred_Callback(hObject, eventdata, handles)
+% hObject    handle to Blurred (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global im_orginal;
+blurred = imgaussfilt(im_orginal, 5);
+im_blurred = imshowpair(im_orginal, blurred, 'montage');
+axes(handles.axes6);
+imshow(im_blurred);
+%handles.o = im_blurred;
+%guidata(hObject, handles);
+%oimg=getimage(handles.axes6);
+%H=fspecial('motion',20,45);
+%motion_img=imfilter(im_orginal,H,'replicate');
+%axes(handles.axes6)
+%imshow(motion_img);
+
+% --- Executes on button press in Unshape_Mask.
+function Unshape_Mask_Callback(hObject, eventdata, handles)
+% hObject    handle to Unshape_Mask (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in Higher_Image.
+function Higher_Image_Callback(hObject, eventdata, handles)
+% hObject    handle to Higher_Image (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in Final_Image.
+function Final_Image_Callback(hObject, eventdata, handles)
+% hObject    handle to Final_Image (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
